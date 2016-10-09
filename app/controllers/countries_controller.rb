b60938dcac6f2f8186019f4f83a0f10663558894
Charles Huang <charleshuang80@gmail.com>
2016-10-09 09:20:11 -0400

    get other products to pass through

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,7 +5,8 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
-    get_top_concerns(@country)
+    set_top_concerns(@country)
+    set_products_by_section(@country)
   end
 
   private
@@ -13,7 +14,7 @@ class CountriesController < ApplicationController
   def assemble_wildlife_product_info(country)
   end
 
-  def get_top_concerns(country)
+  def set_top_concerns(country)
     @top_concerns = []
     country.country_issues.each_with_index do |issue, index|
       if issue.top_concern
@@ -25,5 +26,24 @@ class CountriesController < ApplicationController
       end
     end
     @top_concerns
+  end
+
+  def set_products_by_section(country)
+    # we should be assembling product groups into the sections but for the prototype
+    # product groups are set up like sections
+    country.product_groups.each do |product_group|
+      case product_group.name
+      when 'Food'
+        @food_products = product_group.products
+      when 'Art & Decor'
+        @art_decor_products = product_group.products
+      when 'Clothing & Accessories'
+        @clothing_accessories_products = product_group.products
+      when 'Animals'
+        @animals_products = product_group.products
+      when 'Plants'
+        @plants_products = product_group.products
+      end
+    end
   end
 end
