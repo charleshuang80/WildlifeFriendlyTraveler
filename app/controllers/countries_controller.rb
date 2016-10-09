002d3c584153bcecc7085c98b117a64a80d4ce72
Charles Huang <charleshuang80@gmail.com>
2016-10-09 01:29:45 -0400

    can see top concerns and images (but they need formatting)

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,8 +4,26 @@ class CountriesController < ApplicationController
   end
 
   def show
-    
+    @country = Country.find(params[:id])
+    get_top_concerns(@country)
   end
 
   private
+
+  def assemble_wildlife_product_info(country)
+  end
+
+  def get_top_concerns(country)
+    @top_concerns = []
+    country.country_issues.each_with_index do |issue, index|
+      if issue.top_concern
+        if issue.issueable_type == 'AnimalGroup'
+          @top_concerns << country.animal_groups[index]
+        else
+          # for our prototype there are no ProductGroup top concerns
+        end
+      end
+    end
+    @top_concerns
+  end
 end
